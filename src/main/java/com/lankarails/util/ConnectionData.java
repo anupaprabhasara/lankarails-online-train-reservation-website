650066bf8d8d6d4790fcb49be7e90bc5078d0926
anupaprabhasara <info@anupa.lk>
2025-05-07 09:36:17 +0530

    Innitial Setup Done

---
+++ src/main/java/com/lankarails/util/ConnectionData.java
@@ -0,0 +1,8 @@
+package com.lankarails.util;
+
+public class ConnectionData {
+
+    public static final String DATABASE = "lankarails_db";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "123456";
+}
